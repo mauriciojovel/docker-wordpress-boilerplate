63f5cafe37e0c5c826e6d16c7f976cd6145d93aa
Mauricio Jovel <mjovel@applaudostudios.com>
2017-11-27 19:04:18 -0600

    Adding theme generator

---
+++ src/themes/create-theme.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+read -p "Client Theme name: " client_theme_name
+read -p "Theme php package: " php_package
+read -p "Theme functions prefix: " function_prefix
+read -p "i18 prefix: " i18n_prefix
+read -p "Client url: " client_url
+read -p "Client port: " client_port
+cp -r client-bootstrap $client_theme_name
+find $client_theme_name -type f -print0 | xargs -0 sed -i "s/__php_package__/$php_package/g"
+find $client_theme_name -type f -print0 | xargs -0 sed -i "s/client_theme_bootstrap/$function_prefix/g"
+find $client_theme_name -type f -print0 | xargs -0 sed -i "s/client-theme-bootstrap/$i18n_prefix/g"
+sed -i "s/client-bootstrap/$client_theme_name/g" ../../docker-compose.yml
+sed -i "s/nginx:local.dev/nginx:$client_url/g" ../../docker-compose.yml
+sed -i "s/80:80/$client_port:$client_port/g" ../../docker-compose.yml
+sed -i "s/listen 80/listen $client_port/g" ../../docker/nginx/default.conf
+sed -i "s/local.dev/$client_url:$client_port/g" "$client_theme_name/src/gulpfile.js"
