9d9b464203b413aa533d37814473f05790c5c92e
Mauricio Jovel <mjovel@applaudostudios.com>
2017-11-27 19:29:27 -0600

    Adding support to change the theme title

--- src/themes/create-theme.sh
+++ src/themes/create-theme.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-read -p "Client Theme name: " client_theme_name
+read -p "Client Theme directory: " client_theme_name
+read -p "Client Theme title: " client_title
 read -p "Theme php package: " php_package
 read -p "Theme functions prefix: " function_prefix
 read -p "i18 prefix: " i18n_prefix
@@ -7,6 +8,7 @@ read -p "Client url: " client_url
 read -p "Client port: " client_port
 cp -r client-bootstrap $client_theme_name
 find $client_theme_name -type f -print0 | xargs -0 sed -i "s/__php_package__/$php_package/g"
+find $client_theme_name -type f -print0 | xargs -0 sed -i "s/Client Bootstrap Theme/$client_title/g"
 find $client_theme_name -type f -print0 | xargs -0 sed -i "s/client_theme_bootstrap/$function_prefix/g"
 find $client_theme_name -type f -print0 | xargs -0 sed -i "s/client-theme-bootstrap/$i18n_prefix/g"
 sed -i "s/client-bootstrap/$client_theme_name/g" ../../docker-compose.yml
